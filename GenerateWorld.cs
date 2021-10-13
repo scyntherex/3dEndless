b362127d95642038688c53abb034ec60fd869eb9
scyntherex <rone.caballero@gmail.com>
2021-10-12 23:22:59 -0700

    Added gui.

--- GenerateWorld.cs
+++ GenerateWorld.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GenerateWorld : MonoBehaviour
 {
     static public GameObject dummyTraveler;
     static public GameObject lastPlatform;
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
 
     void Awake()
     {
---
+++ MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("ScrollingWorld", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            QuitGame();
+        }
+    }
+}
