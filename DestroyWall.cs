c94f3dcf82126d1cb5a74b8b8173bdaaccc69063
scyntherex <rone.caballero@gmail.com>
2021-10-22 22:50:10 -0700

    Added explosion fx.

--- DestroyWall.cs
+++ DestroyWall.cs
@@ -8,6 +8,8 @@ public class DestroyWall : MonoBehaviour
     List<Rigidbody> bricksRBs = new List<Rigidbody>();
     List<Vector3> positions = new List<Vector3>();
     List<Quaternion> rotations = new List<Quaternion>();
+    public GameObject explosion;
+
     Collider col;
 
     void OnEnable()
@@ -36,6 +38,8 @@ public class DestroyWall : MonoBehaviour
     {
         if (other.gameObject.tag == "Burger")
         {
+            GameObject obj = Instantiate(explosion, other.contacts[0].point, Quaternion.identity);
+            Destroy(obj, 2.5f);
             col.enabled = false;
             foreach(Rigidbody r in bricksRBs)
             {
